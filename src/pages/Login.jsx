a029f9415e7a6e382542e28c6e7cba31ae3e2bbb
raphael-caninde <raphael.caninde@gmail.com>
2021-09-24 01:27:41 -0300

    estilizando o formulario

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,32 +1,33 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebookF, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import '../style/LoginCss.css';
 
 export default class Login extends Component {
   render() {
     return (
-      <main>
+      <main className='container'>
         <h2>Login</h2>
         <form>
-          <div>
+          <div className='input-field'>
             <input type="text" name='username' id='username' placeholder="Enter your username" />
             <div className="underline"></div>
           </div>
-          <div>
+          <div className='input-field'>
             <input type="text" name='password' id='password' placeholder="Enter your password" />
             <div className="underline"></div>
           </div>
           <input type="submit" value="Continue" />
           </form>
 
-          <div>
+          <div className='footer'>
             <span>Or Conect With Social Media</span>
-            <div className='social-fields'>
-              <div>
+            <div className='social-field'>
+              <div className='facebook'>
                 <FontAwesomeIcon icon={faFacebookF} />
                 Sign in with Facebook
               </div>
-              <div>
+              <div className="instagram">
                 <FontAwesomeIcon icon={faInstagram} />
                 Sign in with Instagram
               </div>
